73e1bcc635804df5d301ec4fe4b9e0a217cba4a9
Arun Kumar <arunkumarm1995@gmail.com>
2016-09-10 21:05:45 +0530

    Bind view to view model, calculate ascii score

--- Week 0 - Introduction/whats-your-ascii/scripts/app.js
+++ Week 0 - Introduction/whats-your-ascii/scripts/app.js
@@ -1,5 +1,17 @@
 (function(){
   'use strict';
   
-  angular.module('NameAsciiApp', []);
+  angular.module('NameAsciiApp', [])
+  
+    .controller('MainController', function($scope){
+      $scope.name = "";
+      $scope.calculate = function () {
+        $scope.value = 0;
+        for (var idx = 0; idx < $scope.name.length; idx++) {
+          $scope.value += $scope.name.charCodeAt(idx);
+        }
+          
+      };
+      
+    });
 }());
