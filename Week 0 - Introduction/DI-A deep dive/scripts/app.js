4420694e01ff20644f4b6cb4f714b9a621c87456
Arun Kumar <arunkumarm1995@gmail.com>
2016-09-10 23:32:51 +0530

    Experiment with DI inference

---
+++ Week 0 - Introduction/DI-A deep dive/scripts/app.js
@@ -0,0 +1,22 @@
+(function(){
+  angular.module('Injector', [])
+    .controller('BaseController', controllerFunc);
+    
+    function controllerFunc($scope, $filter, $injector) {
+      $scope.name = "Arun";
+      $scope.upper = upper;
+      
+      /**
+        $scope, $filter and $injector are all services
+        The below line is used by Angular to infer the services required by a controller and to inject the right service at the right spot
+        This does not work when code is minified of obfuscated as argument names get changed. For more, see https://docs.angularjs.org/api/auto/service/$injector
+      **/
+      var services = $injector.annotate(controllerFunc);
+      console.log(services);
+      
+      function upper() {
+        var upperCase = $filter('uppercase');
+        $scope.name = upperCase($scope.name);
+      }
+    }
+}());
