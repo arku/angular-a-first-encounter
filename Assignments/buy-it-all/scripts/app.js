b75fb368647c3a7d7fff9ecc2b062f3e1dde9066
Arun Kumar <arunkumarm1995@gmail.com>
2016-09-21 08:07:08 +0530

    Seed sample data from service

--- Assignments/buy-it-all/scripts/app.js
+++ Assignments/buy-it-all/scripts/app.js
@@ -1,10 +1,47 @@
-(function(){
-  'use strict';
-  
-  angular.module('ShoppingList', [])
-    .controller('MainController', MainController);
+(function() {
+  angular.module('ShoppingListApp', [])
+    .controller('MainController', MainController)
+    .service('ShoppingList', ShoppingListService);
     
-    function MainController() {
+    MainController.$inject = ['ShoppingList'];
+    
+    function MainController(shoppingList) {
       
+      this.items = shoppingList.getItems();
+    };
+    
+    
+    function ShoppingListService() {
+      var service = this;
+      
+      var items = [
+        {
+          name: 'Cookies',
+          quantity: 15
+        },
+        {
+          name: 'Cool drinks',
+          quantity: 10
+        },
+        {
+          name: 'Eggs',
+          quantity: 30
+        },
+        {
+          name: 'Bread',
+          quantity: 3
+        },
+        {
+          name: 'Chocolates',
+          quantity: 50
+        }
+      ];
+      console.log(items);
+      
+      service.getItems = function() {
+        return items;
+      }
     }
+    
+    
 }());
