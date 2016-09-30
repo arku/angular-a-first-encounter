f78c0b15acec9c69b79738d3e36d01c027d023ab
Arun Kumar <arunkumarm1995@gmail.com>
2016-09-30 14:30:15 +0530

    Add search field and minimal styling

--- Assignments/restaurant-menu-search/scripts/app.js
+++ Assignments/restaurant-menu-search/scripts/app.js
@@ -5,6 +5,12 @@
     .controller('MainController', MainController);
 
   function MainController() {
+    var ctrl = this;
+
+    ctrl.searchDesc = "";
+    ctrl.searchMenu = function() {
+      // TODO: Fetch data from the server
+    }
   }
 
 }());
