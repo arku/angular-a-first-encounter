e44f51c5fcae43385f37fbd6713e659633edcc3d
Arun Kumar <arunkumarm1995@gmail.com>
2016-10-04 09:29:24 +0530

    Fetch data only the first time and reuse it for the subsequet searches

--- Assignments/restaurant-menu-search/scripts/app.js
+++ Assignments/restaurant-menu-search/scripts/app.js
@@ -6,27 +6,59 @@
     .service('SearchService', SearchService);
 
   MainController.$inject = ['SearchService'];
-  function MainController(SearchService) {
+  function MainController(searchService) {
     var ctrl = this;
 
+    ctrl.dataFetched = false;
     ctrl.searchDesc = "";
+
     ctrl.searchMenu = function(searchDesc) {
-      var promise = SearchService.searchMenu();
-      promise.then(function(response) {
-        ctrl.items = response.data.menu_items;
-      });
-    };
+        if(!ctrl.dataFetched) {
+          var promise = searchService.getMenuItems();
+          promise
+            .then(function(response) {
+              var menuItems = response.data.menu_items;
+              console.log('searching for', searchDesc);
+              ctrl.items = searchService.searchMenu(searchDesc, menuItems);
+              ctrl.dataFetched = true;
+            });
+        }
+        else {
+          console.log('data already fetched');
+          ctrl.items = searchService.searchMenu(searchDesc);
+        }
+      };
   }
 
   SearchService.$inject = ['$http'];
   function SearchService($http) {
     var service = this;
+    var menuItems = [];
+    var matchingItems;
 
-    this.searchMenu = function() {
+    service.getMenuItems = function() {
       return $http({
         url: 'https://davids-restaurant.herokuapp.com/menu_items.json'
       });
     };
+
+    service.searchMenu = function(searchDesc, items) {
+      if(items != null) {
+        console.log('Data loaded for the first and last time');
+        items.forEach(function(item) {
+          menuItems.push(item);
+        });
+      }
+
+      matchingItems = [];
+      menuItems.forEach(function(menuItem){
+        if(menuItem.description.indexOf(searchDesc) >= 0)
+          matchingItems.push(menuItem);
+      });
+
+      return matchingItems;
+    };
+
   }
 
 }());
