f49c6b895cc76357a621df8de07b2b649966939c
Arun Kumar <arunkumarm1995@gmail.com>
2016-10-10 02:44:21 +0530

    Add a home template

--- Assignments/grub-showcase/scripts/app.js
+++ Assignments/grub-showcase/scripts/app.js
@@ -1,5 +1,17 @@
 (function() {
   'use strict';
   
-  angular.module('GrubShowcase', []);
+  angular.module('GrubShowcase', ['ui.router'])
+  .config(RoutesConfig);
+
+  RoutesConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
+  function RoutesConfig($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/');
+
+    $stateProvider
+      .state('home', {
+        url: '/',
+        templateUrl: 'views/home.html'
+      });
+  }
 }())
